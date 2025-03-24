5cf5b9a1eea6bc00baac731e0404cf5af7c9799b
Katherine Karpova <katherinekarpova922@gmail.com>
2025-03-24 13:49:06 -0400

    bgvfdascd

--- insert_category_budgets.py
+++ insert_category_budgets.py
@@ -14,40 +14,49 @@ def main():
         'beauty': 300,
         'needed_misc': 150,
         'optional_misc': 100,
-        'business_expenses': None
+        'business_expenses': None,
+        'app_subscriptions': 100
     }
 
-    # create table for each category
-    for category in categories_and_budgets:
+    # create table for categories
+    try:
+        c.execute('''CREATE TABLE IF NOT EXISTS categories (
+                  id INTEGER PRIMARY KEY,
+                  category_name TEXT
+                  )''')
+    except Exception as e:
+        print(e)
+        sys.exit()
+    
+    # create budgets table
+    try:
+        c.execute('''CREATE TABLE IF NOT EXISTS budgets (
+            category_id INTEGER,
+            budgeted_amount FLOAT,
+            FOREIGN KEY (category_id) REFERENCES categories(id)
+            )''')
+    except Exception as e:
+        print(e)
+        sys.exit()
+
+    category_id = 1
+    # insert categories_and_budgets dict into budgets table
+    for category, budgeted_amount in categories_and_budgets.items():
         try:
+            # create table for each category
             c.execute(f'''CREATE TABLE IF NOT EXISTS "{category}" (
             item TEXT,
             price FLOAT
             )''')
+            # insert each category as a row in categories table
+            c.execute('''INSERT INTO categories (id, category_name) VALUES (?, ?)''', (category_id, category,))
+            # insert categories_and_budgets dict into budgets table with category as id num
+            c.execute('''INSERT INTO budgets (category_id, budgeted_amount)
+                        VALUES (?, ?)''', (category_id, budgeted_amount,))
+            category_id += 1
         except Exception as e:
-            print(f'Error creating table for {category}')
             print(e)
-    
-    # create budgets table
-    try:
-        c.execute('''CREATE TABLE IF NOT EXISTS "categories_budgets" (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            category_name TEXT,
-            budgeted_amount FLOAT
-            )''')
-    except Exception as e:
-        print('error creating categories_budgets table')
-        print(e)
-        sys.exit()
-
-    # insert categories_and_budgets dict into budgets table
-    for category, budgeted_amount in categories_and_budgets.items():
-        try:
-            c.execute('''INSERT INTO categories_budgets (category_name, budgeted_amount)
-                        VALUES (?, ?)''', (category, budgeted_amount))
-        except Exception as e:
-            print(f'Error inserting row for {category} in categories_budgets')
-            print(e)
+            sys.exit()
     print(':)')
     conn.commit()
     conn.close()
